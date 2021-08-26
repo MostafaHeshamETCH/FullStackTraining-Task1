a09d24e306fe7fff64fbe754ae45287ab99e3a25
Etch <mostafah2000@live.com>
2021-08-27 00:33:28 +0200

    Adding routing and all views (home, blogs, create blog, ...)

---
+++ README.md
@@ -0,0 +1,6 @@
+#Valoro full stack training - Task 1
+
+This small project consits of:
+- NodeJS + Express backend server connected to MongoDB using Mongoose that
+fetch data from a custom RapidAPI using AXIOS.
+- Very simple EJS frontend to show that data.
--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 //requires
 const express = require('express');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const axios = require('axios');
 
 //express init
 const app = express();
+
+//API handling (axios)
+let clients = [];
+
+(async function getNames() {
+  try {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    clients = data.map(client => client.name);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 //db connection handling
 const dbURI = "mongodb+srv://Etchos:x5dIGf4P3gk5LoUG@cluster0.8vmec.mongodb.net/NodeJS-CrashCourse?retryWrites=true&w=majority"
@@ -11,6 +27,7 @@ mongoose.connect(dbURI, {useNewUrlParser: true, UseUnifiedTopology: true}).then(
   (result) => {
     console.log("connected to db");
     app.listen(3000);
+    console.log("server on port 3000");
   }
 ).catch(
   (err) => {
@@ -25,4 +42,53 @@ app.set('view engine', 'ejs');
 //public folder made public
 app.use(express.static('public'))
 
+//middleware for encoding to accept form data
+app.use(express.urlencoded({extended: true}));
+
 //routes - TODO
+app.get('/', (req, res) => {
+  Blog.find().sort({createdAt: -1}).then(
+    (result) => {
+      res.render('index', { title: 'All Blogs', blogs: result});
+    }
+  ).catch(
+    (err) => {
+      console.log(err);
+    }
+  );
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/api', (req, res) => {
+  res.render('check_api', { title: 'Check API' });
+});
+
+//blog routes
+app.get('/blogs', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save().then(
+    (result) => {
+      res.redirect('/');
+    }
+  ).catch(
+    (err) => {
+      console.log(err);
+    }
+  );
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+  
+// 404 page (must be last) - use function is executed for every request
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
